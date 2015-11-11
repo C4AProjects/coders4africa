7a68993eab6f718a4a65f7655e59d704aa65174e
Haythem <haythem.horbit@gmail.com>
2015-11-11 20:07:36 +0100

    Initial Import & restructering code

--- web/app/app.cfg.js
+++ web/app/app.cfg.js
@@ -1,14 +1,14 @@
 
 
-var config_dev={
-
-    "API_URI": "http://c4africa-dev.c4asolution.com:5000/api/v1.0/"
-}
 var config_prod={
+
+    "API_URI": "http://c4africa-dev.c4asolution.com:5000/api/v1.0"
+}
+var config_dev={
 
     "API_URI": "http://localhost:5000/api/v1.0"
 }
 
 
 
-APP.constant("CONFIG",config_dev )
+APP.constant("CONFIG",config_prod )
