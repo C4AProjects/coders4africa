168522bd3d148eefe7a06e71e34c0432ec5803aa
Haythem <haythem.horbit@gmail.com>
2015-11-12 16:25:18 +0100

    update

--- web/app/registerCtrl.js
+++ web/app/registerCtrl.js
@@ -10,7 +10,7 @@ APP.controller('registerCtrl', ['$rootScope', '$scope', '$http', 'CONFIG', 'Noti
         var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
         return re.test(email);
     }
-    reCAPTCHA.setPublicKey('---KEY---');
+    //reCAPTCHA.setPublicKey('---KEY---');
     $scope._user = {}
     $scope._user.job = ""
     $scope._user.jobType = ""
